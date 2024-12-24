aea693172766fe2ae4614dc23aa732674992bfae
Manikantareddysareddy <manikantareddysareddy@gmail.com>
2024-12-24 14:22:07 +0530

    calculator

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Calculator from './components/Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+      <Calculator/>
+      <h2>Calc by Sareddy Manikantareddy</h2>
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
